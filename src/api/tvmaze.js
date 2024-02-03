88133b04e4d30243848202a932423de7ba597933
Suman Mandal <36975298+suman2mandal@users.noreply.github.com>
2024-02-03 16:32:21 +0530

    Update tvmaze.js

--- src/api/tvmaze.js
+++ src/api/tvmaze.js
@@ -2,9 +2,7 @@ import axios from 'axios';
 const base = "http://api.tvmaze.com";
 
 const apiget = async (queryString) => {
-    const res = await axios.get(base+queryString, {
-        withCredentials: false
-    })
+    const res = await axios.get(base+queryString)
     // console.log("here am i",res.data)
     return (res.data);
     // return "hi";
